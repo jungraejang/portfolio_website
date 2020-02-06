9ac64db5793f5f6d9f97b34b78cf0f0c86f132b6
jungraejang <jungraejang@pursuit.org>
2020-02-06 00:56:46 -0500

    hashrouter basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <HashRouter hashType={"noslash"}>
+  <HashRouter basename="/">
     <App />
   </HashRouter>,
   document.getElementById("root")
